08d6a0693e2a8a086fe9631a55acff771809e689
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2026-04-11 21:33:07 -0600

    (musthy) scales

--- app/pages/musthy/js/scalelist.js
+++ app/pages/musthy/js/scalelist.js
@@ -1,10 +1,46 @@
 export function list_scales(edo, include_transpositions = false) {
 
-    console.log(edo, include_transpositions);
+    console.log("Edo:", edo, "Include transpositions?:", include_transpositions);
+    console.log("Generating scales...");
+
+    // math functions to help prepare list size
+    const factorial = (num) => {
+        if (num == 0) {
+            return 1;
+        }
+        return num * factorial(num - 1);
+    }
+    const combinations = (num, take) => {
+        return factorial(num) / (factorial(take) * factorial(num - take));
+    }
+
+    // calc how many scales we need to make room for in memory
+    const calc_list_size = (edo, include_transpositions) => {
+        let size = 0;
+        for (let ii = 0; ii < (edo); ii++) {
+            if (include_transpositions) {
+                size += combinations(edo, ii);
+            }
+            else {
+                size += combinations(edo - 1, ii);
+            }
+            
+        }
+        return size;
+    }
+    const scale_list_size = calc_list_size(edo, include_transpositions);
+
+    console.log("there will be", scale_list_size, "scales.");
 
     // init scale list
+    let scale_list = new Array(scale_list_size).fill(['x'.repeat(edo)]);
     // include the empty set
-    let scale_list = [[]];
+    scale_list[0] = [];
+
+    // count so we know where to insert new arrays
+    let scale_list_index = 0;
+    // increment because we filled first with empty array
+    scale_list_index++;
     
     // temp variables for calculations
     let unbranched = [];
@@ -37,13 +73,16 @@ export function list_scales(edo, include_transpositions = false) {
             unbranched.push(Array.from(temporary));
         }
         unbranched.shift();
-        scale_list.push(Array.from(P));
+        scale_list[scale_list_index] = (Array.from(P));
+        scale_list_index++;
     };
 
     // generate scales until all "branches" have been made and unbranched is empty
-    while (unbranched.length > 0) {
+    while (unbranched[0]) {
         branch(unbranched[0]);
+        console.log(scale_list.length)
     };
 
+    console.log("finished generating scales!")
     return scale_list;
 }
--- app/pages/musthy/scales.vue
+++ app/pages/musthy/scales.vue
@@ -4,7 +4,8 @@
     <h1>Musical Scale Calculator</h1>
     <p>
       Calculate the pc sets of every music scale in an equal temperment of your
-      choice!
+      choice! Right now, things don't really work for the bigger scales. Sorry,
+      but it will break D:
     </p>
     <div class="user-options">
       <div>
@@ -22,6 +23,16 @@
     </div>
     <button @click="generateScales()">Calculate Scales!</button>
     <hr />
+    <div v-if="done_generating">
+      <template v-for="ele in scale_list">
+        <p>{{ ele }}</p>
+      </template>
+    </div>
+    <div v-else>
+      <p>
+        {{ generating_scales ? "Generating..." : "Click to generate scales!" }}
+      </p>
+    </div>
   </div>
 </template>
 
@@ -34,8 +45,17 @@ const edo = ref(12);
 
 const include_transpositions = ref(false);
 
+const generating_scales = ref(false);
+const done_generating = ref(false);
+
+const scale_list = ref([]);
+
 const generateScales = () => {
-  const scale_list = list_scales(edo.value, include_transpositions.value);
+  generating_scales.value = true;
+
+  scale_list.value = list_scales(edo.value, include_transpositions.value);
+  done_generating.value = true;
+  generating_scales.value = false;
   console.log(scale_list);
 };
 </script>
