266c0b58351ea2a3ca93575ec467dbeff1ff88ee
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2026-04-12 22:56:30 -0600

    (musthy) added function to play the scales

--- app/pages/musthy/scales.vue
+++ app/pages/musthy/scales.vue
@@ -43,9 +43,28 @@
     </button>
     <p>Generated {{ scale_amount }} scales.</p>
     <hr />
-    <div v-if="generation_state == done">
+    <div v-if="generation_state == done" class="scales">
       <template v-for="ele in scale_list">
         <p>{{ ele }}</p>
+        <button
+          @click="
+            () => {
+              playScale(ele);
+            }
+          "
+        >
+          Play
+        </button>
+        <button
+          @click="
+            () => {
+              playScale(ele, true);
+            }
+          "
+        >
+          Chord
+        </button>
+        <br />
       </template>
     </div>
     <div v-else>
@@ -58,8 +77,10 @@
 import { watch } from "vue";
 import Header from "/components/Header.vue";
 import { list_scales } from "./js/scalelist";
+import useSimpleTone from "/composables/simpleTones.js";
 
-const max_edo = ref(22);
+// max_edo is dependent on how large things can be before website breaks D:
+const max_edo = ref(17);
 const edo = ref(12);
 
 const include_transpositions = ref(false);
@@ -87,6 +108,38 @@ const generateScales = () => {
   scale_amount.value = scales.size;
   console.log(scale_list);
 };
+
+const playScale = async (scale, isChord = false) => {
+  const tonic = 261.63;
+
+  const freq = (ele) => {
+    if (ele == 0) {
+      return tonic;
+    } else {
+      return (
+        Math.round(
+          (Math.pow(2, ele / edo.value) * tonic + Number.EPSILON) * 100,
+        ) / 100
+      );
+    }
+  };
+
+  // stolen from below link to wait for an amount of time (to play arpeggio style).
+  // https://stackoverflow.com/questions/64387549/wait-for-settimeout-to-complete-then-proceed-to-execute-rest-of-the-code
+  function wait(seconds) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, seconds * 1000);
+    });
+  }
+
+  for (let ii = 0; ii < scale.length; ii++) {
+    useSimpleTone().playTone(freq(scale[ii]));
+    if (!isChord) {
+      await wait(0.5);
+      console.log("hi");
+    }
+  }
+};
 </script>
 
 <style scoped>
@@ -94,5 +147,18 @@ const generateScales = () => {
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
+}
+
+.scales {
+  display: grid;
+  flex-direction: row;
+  justify-content: center;
+}
+.scales * {
+  text-align: center;
+}
+.scales button {
+  width: 200px;
+  justify-content: center;
 }
 </style>
