4ce24aa9aa13888d79b5b206cf769e9dbd394767
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2026-06-19 22:51:41 -0600

    (musthy) different approach to scale ID systems page.

--- app/pages/musthy/idsystem/index.vue
+++ app/pages/musthy/idsystem/index.vue
@@ -4,6 +4,12 @@
       <h1>ID Systems for Musical Scales</h1>
       <p>by Luke Knotts</p>
       <hr />
+      <p>
+        When making a website, having a numeric ID for each musical scale is
+        useful. <i>Ian Ring</i> used binary representations of musical scales on
+        <a href="https://ianring.com/musictheory/">their website</a>. An example
+        of the system is below.
+      </p>
     </Layout>
   </div>
 </template>
