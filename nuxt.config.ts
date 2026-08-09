05de2e18c8d94efbadb0e062a84753464a2e2d7a
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2026-08-09 17:00:22 -0600

    Config for Nuxt Fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 export default defineNuxtConfig({
   // last troubleshooted with npm 9 August 2026. Google's AI said this was okay to have, but idk what this is really
   compatibilityDate: '2026-08-09',
+  modules: ['@nuxt/fonts',]
 })
