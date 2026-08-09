dece398ea7916d5854b7af647758d88f299b8b82
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2026-08-09 16:45:15 -0600

    Adding comment to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
 export default defineNuxtConfig({
+  // last troubleshooted with npm 9 August 2026. Google's AI said this was okay to have, but idk what this is really
   compatibilityDate: '2026-08-09',
 })
